efe0f18a29aecf4c20b4cb957f79bf32ff85dc93
savasw <sapan.vaswani@click2cloud.net>
2017-05-26 15:30:21 +0530

    added dependency and shifted to environment variable

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 var config = {};
 
 config.Azuresearch = {}
+config.Postgres = {}
 
 //Azure search API key
-config.Azuresearch.apiKey = "25843E690EC7F3A80FBF74DD67641D7E";
-//config.apiKey = process.env.searchServicePrimaryKey
+config.Azuresearch.apiKey = process.env.searchServicePrimaryKey;
 
 //Azure search service URL
-config.Azuresearch.serviceURL = "https://akash.search.windows.net";
-//config.serviceURL = process.env.searchServiceUri
+config.Azuresearch.serviceURL = process.env.searchServiceUri;
 
 //Azure search index name to be created
 config.Azuresearch.indexName = "azure-search-data";
@@ -16,12 +15,8 @@ config.Azuresearch.indexName = "azure-search-data";
 //Azure search serive API version
 config.Azuresearch.apiVersion = "2015-02-28-Preview";
 
-
-
-config.Postgres = {}
 //Postgres database connection string
+config.Postgres.connstr = process.env.POSTGRESQLCONNSTR_DefaultConnection;
 
-//config.db.connstr = process.env.POSTGRESQLCONNSTR_DefaultConnection;
-config.Postgres.connstr = "postgres://postgres@postgresqlq7zavhy5ohzzy:pg%4012345@postgresqlq7zavhy5ohzzy.postgres.database.azure.com:5432/microsoft?ssl=true";
 
 module.exports = config;
